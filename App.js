01521c3d606224bb2d6b9b529123968c688b5686
Loka Kanarp <lokakanarp@gmail.com>
2020-04-18 10:49:26 +0200

    Added the spinner.

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, ActivityIndicator, StatusBar } from 'react-native';
 import Movie from './components/Movie';
 
 export default function App() {
 
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-
-  let shop = [
-    {id: 35, name: 'jumper', color: 'red', price: 20},
-    {id: 42, name: 'shirt', color: 'blue', price: 15},
-    {id: 56, name: 'pants', color: 'green', price: 25},
-    {id: 71, name: 'socks', color: 'black', price: 5},
-    {id: 72, name: 'socks', color: 'white', price: 5},
-  ]
 
   useEffect(() => {
     fetch('https://ghibliapi.herokuapp.com/films/')
@@ -26,9 +18,10 @@ export default function App() {
   return (
     <ScrollView style={styles.container}>
       <Text>Lokas fina app!</Text>
-        {data.map((item, key) =>
+        {isLoading ? <ActivityIndicator/> : (
+          data.map((item, key) =>
           <Movie item={item} key={item.id}/>
-        )}
+        ))}
     </ScrollView>
   );
 }
